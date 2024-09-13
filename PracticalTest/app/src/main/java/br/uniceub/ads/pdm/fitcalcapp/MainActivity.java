c638f1551d700eb6b8bbc470993d6d4bb0d66fd6
Enzo de Oliveira Passos Correia <Enzo.OPC1@gmail.com>
2024-09-13 11:18:25 -0300

    Create Objects
    Creating the objects that will be used an import the respective classes

--- PracticalTest/app/src/main/java/br/uniceub/ads/pdm/fitcalcapp/MainActivity.java
+++ PracticalTest/app/src/main/java/br/uniceub/ads/pdm/fitcalcapp/MainActivity.java
@@ -1,6 +1,11 @@
 package br.uniceub.ads.pdm.fitcalcapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,75 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Views da tela principal
+
+    public Button tela_principal_btn_calculadora_imc;
+
+    public Button tela_principal_btn_calculadora_peso;
+
+    public Button tela_principal_btn_calculadora_altura;
+
+    // Views da tela para calcular o IMC
+
+    public Button tela_calculadora_imc_btn_calcular;
+
+    public Button tela_calculadora_imc_btn_voltar;
+
+    public RadioGroup tela_calculadora_imc_radio_group;
+
+    public RadioButton tela_calculadora_imc_radio_masculino;
+
+    public RadioButton tela_calculadora_imc_radio_feminino;
+
+    public EditText tela_calculadora_imc_txt_altura;
+
+    public TextView tela_calculadora_imc_resultado;
+
+
+    // Views da tela para calcular o peso ideal
+
+    public Button tela_calculadora_peso_btn_calcular;
+
+    public Button tela_calculadora_peso_btn_voltar;
+
+    public RadioGroup tela_calculadora_peso_radio_group;
+
+    public RadioButton tela_calculadora_peso_radio_masculino;
+
+    public RadioButton tela_calculadora_peso_radio_feminino;
+
+    public EditText tela_calculadora_peso_txt_peso;
+
+    public EditText tela_calculadora_peso_txt_altura;
+
+    public TextView tela_calculadora_peso_resultado;
+
+
+    // Views da tela para calcular a altura ideal
+
+    public Button tela_calculadora_altura_btn_calcular;
+
+    public Button tela_calculadora_altura_btn_voltar;
+
+    public RadioGroup tela_calculadora_altura_radio_group;
+
+    public RadioButton tela_calculadora_altura_radio_masculino;
+
+    public RadioButton tela_calculadora_altura_radio_feminino;
+
+    public EditText tela_calculadora_altura_txt_altura;
+
+    public TextView tela_calculadora_altura_resultado;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.tela_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        
 
             // Just testing smth
         });
