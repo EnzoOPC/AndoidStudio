3261b665214630791341646021d1347e805e3537
Enzo de Oliveira Passos Correia <Enzo.OPC1@gmail.com>
2024-10-02 19:28:13 -0300

    Update
    It doesnt work >:(

--- Cadastro_de_produtos/app/src/main/java/br/uniceub/ads/pdm/cadastro_de_produtos/MainActivity.java
+++ Cadastro_de_produtos/app/src/main/java/br/uniceub/ads/pdm/cadastro_de_produtos/MainActivity.java
@@ -1,6 +1,9 @@
 package br.uniceub.ads.pdm.cadastro_de_produtos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,21 +18,70 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Views tela principal
+
+    public Button btn_Cadastrar;
+
+    // Views tela cadastro
+
+    public EditText nomeProduto;
+
+    public EditText descricao;
+
+    public EditText valor;
+
+    public Button cadastrar;
+
+    List<Produto> produtos = new ArrayList<Produto>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+
+        LoadMain();
+
+    }
+
+    public void LoadMain() {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
-        List<Produto> produtos = new ArrayList<Produto>();
-        produtos.add(new Produto("Enzo", "Um serumaninho legal", 498.3, true, R.drawable.green_v));
-        produtos.add(new Produto("João", "Um serumaninho legal", 498.3, true, R.drawable.red_x));
-        produtos.add(new Produto("Guilherme", "Um serumaninho legal", 498.3, true, R.drawable.green_v));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         recyclerView.setAdapter(new Adapter(getApplicationContext(), produtos));
+
+        btn_Cadastrar = findViewById(R.id.btnCadastrar);
+
+        btn_Cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoadTelaCadastro();
+            }
+        });
+    }
+
+    public void LoadTelaCadastro() {
+        setContentView(R.layout.tela_cadastro);
+
+        cadastrar = findViewById(R.id.buttonSalvar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nomeProduto.getText().toString();
+                String description = descricao.getText().toString();
+                String value = valor.getText().toString();
+
+                double valorDouble = Double.parseDouble(value);
+
+                produtos.add(new Produto(name, description, valorDouble,true, R.drawable.green_v));
+
+                LoadMain();
+            }
+        });
     }
 }
